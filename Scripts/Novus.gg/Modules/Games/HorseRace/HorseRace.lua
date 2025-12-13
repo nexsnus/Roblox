ccddf851327cdab1ef5ed55f9b311540550911f8
nexsnus <Nexusfnbr@proton.me>
2025-12-13 08:42:08 +0100

    Update HorseRace.lua

--- Scripts/Novus.gg/Modules/Games/HorseRace/HorseRace.lua
+++ Scripts/Novus.gg/Modules/Games/HorseRace/HorseRace.lua
@@ -1,20 +1,26 @@
 local Window = _G.Window
 local hrmat = Window:CreateTab("Automation", "home")
 
+local autoWinsFarmActive = false
+local autoRebirthActive = false
+local autoSuperRebirthActive = false
+
 local hrmatawft = hrmat:CreateToggle({
     Name = "Auto Wins Farm",
     CurrentValue = false,
     Flag = "hrmatawft",
     Callback = function(state)
-        while state do
-            game:GetService("ReplicatedStorage").Packages._Index["sleitnick_knit@1.5.1"].knit.Services.RandomPotionService.RE.BuyPotionEvent:FireServer(table.unpack({
-              [1] = -9e60,
-              [2] = {
-              },
-              [3] = true,
-              [4] = "hax",
-            }))
-            wait()
+        autoWinsFarmActive = state
+        if state then
+            while autoWinsFarmActive do
+                game:GetService("ReplicatedStorage").Packages._Index["sleitnick_knit@1.5.1"].knit.Services.RandomPotionService.RE.BuyPotionEvent:FireServer(table.unpack({
+                     = -9e60,
+                     = {},
+                     = true,
+                     = "hax",
+                }))
+                wait(0.05)  
+            end
         end
     end,
 })   
@@ -24,9 +30,12 @@ local hrmatart = hrmat:CreateToggle({
     CurrentValue = false,
     Flag = "hrmatart",
     Callback = function(state)
-        while state do
-            game:GetService("ReplicatedStorage").Packages._Index["sleitnick_knit@1.5.1"].knit.Services.RebirthService.RF.Rebirth:InvokeServer()
-            wait()
+        autoRebirthActive = state
+        if state then
+            while autoRebirthActive do
+                game:GetService("ReplicatedStorage").Packages._Index["sleitnick_knit@1.5.1"].knit.Services.RebirthService.RF.Rebirth:InvokeServer()
+                wait(0.05)  
+            end
         end
     end,
 })   
@@ -36,9 +45,12 @@ local hrmatasrt = hrmat:CreateToggle({
     CurrentValue = false,
     Flag = "hrmatasrt",
     Callback = function(state)
-        while state do
-            game:GetService("ReplicatedStorage").Packages._Index["sleitnick_knit@1.5.1"].knit.Services.RebirthService.RF.SuperRebirth:InvokeServer()
-            wait()
+        autoSuperRebirthActive = state
+        if state then
+            while autoSuperRebirthActive do
+                game:GetService("ReplicatedStorage").Packages._Index["sleitnick_knit@1.5.1"].knit.Services.RebirthService.RF.SuperRebirth:InvokeServer()
+                wait(0.05)  
+            end
         end
     end,
-})   
+})
