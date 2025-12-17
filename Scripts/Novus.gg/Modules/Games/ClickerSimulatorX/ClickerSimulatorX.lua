27e1f8b355898dfa3e2917b473f22e551d2c0305
nexsnus <Nexusfnbr@proton.me>
2025-12-17 18:47:34 +0100

    Update ClickerSimulatorX.lua

--- Scripts/Novus.gg/Modules/Games/ClickerSimulatorX/ClickerSimulatorX.lua
+++ Scripts/Novus.gg/Modules/Games/ClickerSimulatorX/ClickerSimulatorX.lua
@@ -13,24 +13,32 @@ local csxmatakbt = csxmat:CreateToggle({
     CurrentValue = false,
     Flag = "csxmatakbt",
     Callback = function(state)
-        if state then
-            spawn(function()
-                while state do
-                    local children = banane:GetChildren()
-                    if #children > 0 then
-                        for _, boss in ipairs(children) do
-                            if boss:IsA("BasePart") then
-                                humanoidRootPart.CFrame = boss.CFrame
-                                task.wait(0.7)
-                            end
-                        end
-                    end
-                    task.wait(0.5)
-                end
-            end)
-        end
+        local autokillbosss = state
+            if state then
+                autokillboss()
+            end
     end,
 })   
+function autokillboss()
+    spawn(function()
+            local autokillbosss = true
+            while autokillbosss do
+                pcall(function()
+                    local children = banane:GetChildren()
+                        if #children > 0 then
+                            for _, boss in ipairs(children) do
+                                if boss:IsA("BasePart") then
+                                    humanoidRootPart.CFrame = boss.CFrame
+                                    task.wait(0.7)
+                                end
+                            end
+                        end
+                        task.wait(0.5)
+                    end
+                end)
+            end
+    end)
+end
 
 local csxmet = Window:CreateTab("Eggs", "egg")
 
