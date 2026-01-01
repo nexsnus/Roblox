755cb878e7251e39cd671746ccc868e16f0c0c19
nexsnus <Nexusfnbr@proton.me>
2026-01-01 18:23:16 +0100

    Create LowGraphicsMode.lua

---
+++ Scripts/Novus.gg/Modules/Universal/FPS/LowGraphicsMode/LowGraphicsMode.lua
@@ -0,0 +1,51 @@
+local Window = _G.Window
+local ut = _G.ut
+
+local utfpss = ut:CreateSection("FPS")
+local utlgmb = ut:CreateButton({
+    Name = "Low Graphics Mode",
+    Callback = function()
+        local Terrain = workspace:FindFirstChildWhichIsA("Terrain")
+      	Terrain.WaterWaveSize = 0
+      	Terrain.WaterWaveSpeed = 0
+      	Terrain.WaterReflectance = 0
+      	Terrain.WaterTransparency = 1
+      	Lighting.GlobalShadows = false
+      	Lighting.FogEnd = 9e9
+	      Lighting.FogStart = 9e9
+      	settings().Rendering.QualityLevel = 1
+      	for _, v in pairs(game:GetDescendants()) do
+	      	if v:IsA("BasePart") then
+	      		v.CastShadow = false
+		      	v.Material = "Plastic"
+		      	v.Reflectance = 0
+		      	v.BackSurface = "SmoothNoOutlines"
+		      	v.BottomSurface = "SmoothNoOutlines"
+		      	v.FrontSurface = "SmoothNoOutlines"
+		      	v.LeftSurface = "SmoothNoOutlines"
+		      	v.RightSurface = "SmoothNoOutlines"
+		      	v.TopSurface = "SmoothNoOutlines"
+		      elseif v:IsA("Decal") then
+	      		v.Transparency = 1
+		      	v.Texture = ""
+	      	elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+		      	v.Lifetime = NumberRange.new(0)
+		      end
+	      end
+	      for _, v in pairs(Lighting:GetDescendants()) do
+	      	if v:IsA("PostEffect") then
+		      	v.Enabled = false
+		      end
+	      end
+	      workspace.DescendantAdded:Connect(function(child)
+		      task.spawn(function()
+		      	if child:IsA("ForceField") or child:IsA("Sparkles") or child:IsA("Smoke") or child:IsA("Fire") or child:IsA("Beam") then
+			      	RunService.Heartbeat:Wait()
+			      	child:Destroy()
+			      elseif child:IsA("BasePart") then
+			      	child.CastShadow = false
+			      end
+		      end)
+	      end)
+    end,
+})
