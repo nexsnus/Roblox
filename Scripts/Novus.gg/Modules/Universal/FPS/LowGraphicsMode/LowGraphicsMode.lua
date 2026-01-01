29609d238e7029be1cf66de4bbaef48703a5756d
nexsnus <Nexusfnbr@proton.me>
2026-01-01 18:31:14 +0100

    Update LowGraphicsMode.lua

--- Scripts/Novus.gg/Modules/Universal/FPS/LowGraphicsMode/LowGraphicsMode.lua
+++ Scripts/Novus.gg/Modules/Universal/FPS/LowGraphicsMode/LowGraphicsMode.lua
@@ -6,14 +6,15 @@ local utlgmb = ut:CreateButton({
     Name = "Low Graphics Mode",
     Callback = function()
         local Terrain = workspace:FindFirstChildWhichIsA("Terrain")
+		local Lighting = game:GetService("Lighting")
       	Terrain.WaterWaveSize = 0
       	Terrain.WaterWaveSpeed = 0
       	Terrain.WaterReflectance = 0
       	Terrain.WaterTransparency = 1
       	Lighting.GlobalShadows = false
       	Lighting.FogEnd = 9e9
-	      Lighting.FogStart = 9e9
-      	settings().Rendering.QualityLevel = 1
+	    Lighting.FogStart = 9e9
+      	settings().Rendering.QualityLevel = Enum.QualityLevel.Level01
       	for _, v in pairs(game:GetDescendants()) do
 	      	if v:IsA("BasePart") then
 	      		v.CastShadow = false
@@ -28,9 +29,11 @@ local utlgmb = ut:CreateButton({
 		      elseif v:IsA("Decal") then
 	      		v.Transparency = 1
 		      	v.Texture = ""
-	      	elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-		      	v.Lifetime = NumberRange.new(0)
-		      end
+	      	  elseif v:IsA("ParticleEmitter") then
+  				  v.Lifetime = NumberRange.new(0, 0)
+			  elseif v:IsA("Trail") then
+  				  v.Lifetime = 0
+			  end
 	      end
 	      for _, v in pairs(Lighting:GetDescendants()) do
 	      	if v:IsA("PostEffect") then
