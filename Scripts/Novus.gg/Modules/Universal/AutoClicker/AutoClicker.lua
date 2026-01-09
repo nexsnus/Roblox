59924d2d2a8c9198de910ab30a8b9de548dfba92
nexsnus <Nexusfnbr@proton.me>
2026-01-09 14:21:29 +0100

    Update AutoClicker.lua

--- Scripts/Novus.gg/Modules/Universal/AutoClicker/AutoClicker.lua
+++ Scripts/Novus.gg/Modules/Universal/AutoClicker/AutoClicker.lua
@@ -53,7 +53,7 @@ local utacis = ut:CreateSlider({
 
 local utactb = ut:CreateKeybind({
     Name = "Auto Clicker Toggle Bind",
-    CurrentKeybind = "",
+    CurrentKeybind = nil,
     HoldToInteract = false,
     Flag = "utactb",
     Callback = function()
