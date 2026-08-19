902a797a8d992c51877a7cab4974eb2420a36387
nexsnus <Nexusfnbr@proton.me>
2026-08-19 17:46:16 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,12 @@
+queue_on_teleport([=[
+    loadstring("https://raw.githubusercontent.com/nexsnus/Roblox/refs/heads/main/test.lua")()
+]=])
 
+
+print("Warte auf Thomas")
+repeat task.wait() until game:GetService("Players").LocalPlayer.PlayerGui.Main.Notice.Visible
+
+game:GetService("TeleportService"):Teleport(
+    game.PlaceId,
+    game:GetService("Players").LocalPlayer
+)
